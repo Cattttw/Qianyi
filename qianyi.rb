4dffae414b18477977f9c9a6c5a328e1dd851ee9
Cattttw <76734612+Cattttw@users.noreply.github.com>
2020-12-30 23:07:18 +0800

    Add files via upload

---
+++ qianyi.rb
@@ -0,0 +1,43 @@
+
+#read a dataset and show the dataset in a form format
+
+#judge the potential act
+def judgeact(a)
+  if (a=='C')
+    Create()
+  end
+  if (a=='R')
+    Read()
+  end
+  if (a=='U')
+    Update()
+  end
+  if(a=='D')
+    Delete()
+  end
+
+end
+
+def Create()
+  #put the input of a new piece of data to newdata
+  #put the data in the dataset
+end
+
+def Read()
+#to be filled
+end
+
+def Update()
+#to be filled
+end
+
+def Delete()
+#to be filled
+end
+
+#catergorize data
+def organize(a,b)
+  #judge whether the data sets include b in their a column
+  # if some include then put these in the front
+      end
+end
